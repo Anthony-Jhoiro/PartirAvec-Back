f5c2b4de2be35efd64d4f7281fd668f1003d9f18
Anthony <anthony.quere@lacatholille.fr>
2020-06-09 14:00:55 +0200

    link gateway to upload service

--- destinationservice/src/main/java/com/partiravec/destinationservice/controllers/DestinationController.java
+++ destinationservice/src/main/java/com/partiravec/destinationservice/controllers/DestinationController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/dest")
+//@RequestMapping("/dest")
 public class DestinationController {
 
     private final DestinationDao destinationDao;
